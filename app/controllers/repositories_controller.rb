05700bf18eecf9b6cbb4febaa1db4600533e4792
thecarsgone <carolina.nicole.gonzalez@gmail.com>
2016-08-31 10:53:27 -0400

    Done

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,9 +1,16 @@
 class RepositoriesController < ApplicationController
 
   def search
-
   end
 
   def github_search
+    resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+      req.params['q'] = params[:query]
+      req.params['client_id'] = ENV['github_id']
+      req.params['client_secret'] = ENV['github_secret']
+    end
+      @results = JSON.parse(resp.body)['items']
+      render 'search'
   end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def github_url(item)
+    "https://github.com/#{item['full_name']}"
+  end 
+
+
 end

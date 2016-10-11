a4045bb4dbb040dee3cf97aca21c2f2f7e506cd6
satub <satu.barnhill@gmail.com>
2016-10-11 11:17:43 -0400

    E pur si muove

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -28,6 +28,7 @@ require 'support/github_response.rb'
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 WebMock.disable_net_connect!(:allow_localhost => true)
+# WebMock.disable!
 
 RSpec.configure do |config|
   config.include GithubHelpers

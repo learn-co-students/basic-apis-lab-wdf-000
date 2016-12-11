874f57c177f51faf58cea042f9e918f16002de83
Flatiron School <github email address>
2016-12-11 15:23:50 -0500

    done

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,9 +1,26 @@
 class RepositoriesController < ApplicationController
 
   def search
-
   end
 
   def github_search
+    @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+
+       req.params['client_id'] = ENV["github_id"]
+       req.params['client_secret'] = ENV["github_secret"]
+
+       req.params['q'] = params[:query]
+     end
+
+    body_hash = JSON.parse(@resp.body)
+
+    if @resp.success?
+        @items = body_hash["items"]
+      else
+        @error = "error"
+      end
+
+    render 'search'
   end
+
 end
